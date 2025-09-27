20a267899485dcc409ed7d02441aefc67770422d
Shubham Khirwadkar <khirwadkar.shubham25@gmail.com>
2025-09-27 14:29:41 -0500

    logging

--- scripts/commons.py
+++ scripts/commons.py
@@ -1,5 +1,6 @@
 import concurrent.futures
 import ipaddress
+import logging
 import socket
 
 import ping3
@@ -9,16 +10,19 @@ from scapy.volatile import RandShort
 
 
 class Commons:
-    def __init__(self):
+    def __init__(self, name):
+        self.logger = logging.getLogger(self.__class__.__name__)
         self.timeout = 2
         self.max_workers = 50
         self.ports = list(range(1, 10001))
 
     def scan_port(self, host, port):
         try:
+            self.logger.debug("Initializing the socket")
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.settimeout(self.timeout)
 
+            self.logger.debug("Sending the connection.")
             result = s.connect_ex((host, port))
 
             if result == 0:
@@ -28,49 +32,52 @@ class Commons:
                 return False
 
         except socket.gaierror as e:
-            print(f"Hostname could not be resolved. Exiting\nError:\n{e}.")
+            self.logger.debug(f"Hostname could not be resolved. Exiting\nError:\n{e}.")
             return -1
 
         except socket.error as e:
-            print(f"Could not connect to server. Exiting\nError:\n{e}")
+            self.logger.debug(f"Could not connect to server. Exiting\nError:\n{e}")
             return -1
 
         finally:
+            self.logger.debug("Closing the socket")
             s.close()
 
-    @staticmethod
-    def resolve_domain_name(domain):
+    def resolve_domain_name(self, domain):
         try:
-            ipaddress = socket.gethostbyname(domain)
-            return ipaddress
+            self.logger.debug("Getting the hostname using the socket.")
+            ip_address = socket.gethostbyname(domain)
+            return ip_address
 
         except socket.gaierror as e:
-            print(f"Domain name {domain} could not be resolved. Exiting\nError:\n{e}.")
+            self.logger.debug(f"Domain name {domain} could not be resolved. Exiting\nError:\n{e}.")
             return -1
 
         except Exception as e:
-            print(f"An unexcepted error occurred. Exiting\nError:\n{e}")
+            self.logger.debug(f"An unexcepted error occurred. Exiting\nError:\n{e}")
             return -1
 
-    @staticmethod
-    def get_ip_list(ip, start_ip, end_ip):
+    def get_ip_list(self, ip, start_ip, end_ip):
         ip_list = []
         if ip is not None and "/" in ip:
             try:
                 network = ipaddress.ip_network(ip, strict=False)
-                for ip in network.hosts():
-                    ip_list.append(str(ip))
+                for _ip in network.hosts():
+                    ip_list.append(str(_ip))
 
+                self.logger.debug(f"Returning the list of IP Addresses based on the input {ip}")
                 return ip_list
             except ValueError as e:
-                print(f"Invalid Input. Error:\n{e}")
+                self.logger.debug(f"Invalid Input. Error:\n{e}")
 
         elif ip is not None and "/" not in ip:
             try:
                 ip = ipaddress.ip_address(ip)
+
+                self.logger.debug(f"Returning the single IP Address based on the input {ip}")
                 return [ip]
             except ValueError as e:
-                print(f"Invalid Input. Error:\n{e}")
+                self.logger.debug(f"Invalid Input. Error:\n{e}")
                 return []
 
         elif start_ip is not None and end_ip is not None:
@@ -85,13 +92,14 @@ class Commons:
                     ip_list.append(str(start_ip))
                     start_ip = ipaddress.ip_address(int(start_ip) + 1)
 
+                self.logger.debug(f"Returning the list of IP Addresses between start ip: {start_ip} and end ip: {end_ip}")
                 return ip_list
 
             except ValueError as e:
-                print(f"Invalid Input. Error:\n{e}")
+                self.logger.debug(f"Invalid Input. Error:\n{e}")
 
         else:
-            print("No input provided. Please provide input")
+            self.logger.debug("No input provided. Please provide input")
             return -1
 
     def send_pings(self, target_ips):
@@ -105,19 +113,20 @@ class Commons:
 
                 if is_reachable:
                     hosts.append(ip)
-
         return hosts
 
     def ping_ip(self, ip):
         try:
+            self.logger.debug(f"Sending Ping to {ip}")
             delay = ping3.ping(ip, timeout=self.timeout)
 
             is_reachable = isinstance(delay, float)
+            self.logger.debug(f"Ping to IP Address: {ip} is {'reachable.' if is_reachable else 'not reachable.'}")
 
             return ip, is_reachable
 
         except Exception as e:
-            print(f"Error in ping {ip}. Error\n{e}")
+            self.logger.debug(f"Error in ping {ip}. Error\n{e}")
             return  -1, False
 
     def port_scan_stealth_check(self, ip):
@@ -140,9 +149,11 @@ class Commons:
         packet = ip_layer / tcp_layer
 
         try:
+            self.logger.debug(f"Sending packet with ip: {ip} and port:{port}")
             res = sr1(packet, timeout=self.timeout, verbose=0)
 
             if res is None:
+                self.logger.debug("The port status is filtered")
                 return port, "filtered"
 
             if res.haslayer(TCP):
@@ -150,12 +161,16 @@ class Commons:
 
                 if tcp_flags == 0x12:
                     sr1(IP(dst=ip)/TCP(dport=port, flags='R'), timeout=0, verbose=0)
+                    self.logger.debug("The port status is open")
                     return port, "open"
 
                 elif tcp_flags == 0x14:
+                    self.logger.debug("The port status is close")
                     return port, "closed"
+            self.logger.debug("The port status is filtered")
             return port, "filtered"
 
         except Exception as e:
-            print(f"Scapy exception during scan of {ip}:{port}: {e}")
+            self.logger.debug(f"Scapy exception during scan of {ip}:{port}: {e}")
+            self.logger.debug("The port status is error")
             return port, "error"
--- scripts/port_scanning.py
+++ scripts/port_scanning.py
@@ -1,5 +1,6 @@
 import argparse
 import ipaddress
+import logging
 import os
 import platform
 import sys
@@ -9,7 +10,10 @@ from scripts.commons import Commons
 
 class PortFiltering(Commons):
     def __init__(self):
-        super().__init__()
+        super().__init__("Commons Class")
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.debug("Initializing the port scanning script")
+
 
     def main(self):
         self.clear_cli()
@@ -17,33 +21,40 @@ class PortFiltering(Commons):
         option = self.show_all_options()
 
         if option == 0:
-            print('Exiting the scanning')
+            self.logger.debug(f"Option chosen: {option}. Exiting the port scanning script.")
+            print("Exiting the scanning")
 
         if option == 1:
+            self.logger.debug(f"Option chosen: {option}. Scanning all the ports in the system, also known as Vanilla Scan")
             self.scan_all_ports(**cli_args)
 
         if option == 2:
+            self.logger.debug(f"Option chosen: {option}. Resolving the domain name")
             self.resolve_domain(**cli_args)
 
         if option == 3:
+            self.logger.debug(f"Option chosen: {option}. Performing Ping scan")
             self.ping_scan(**cli_args)
 
         if option == 4:
+            self.logger.debug(f"Option chosen: {option}. Performing SYN Scan also know as TCP health scan")
             self.syn_port_scan_stealth_check(**cli_args)
 
-    @staticmethod
-    def clear_cli():
+    def clear_cli(self):
         if platform.system() == 'Windows':
+            self.logger.debug(f"Operating system is {platform.system()}")
             os.system("cls")
+            self.logger.debug("Cleared cli using command: cls")
 
         elif platform.system() == 'Linux' or platform.system() == 'Darwin':
+            self.logger.debug(f"Operating system is {platform.system()}")
             os.system("clear")
+            self.logger.debug("Cleared cli using command: clear")
 
         else:
-            print(f"Unknown operating system: {platform.system()}")
+            self.logger.debug(f"Unknown operating system {platform.system()}")
 
-    @staticmethod
-    def take_cli_options():
+    def take_cli_options(self):
         parser = argparse.ArgumentParser(description="This script is user for scanning the ports")
 
         parser.add_argument(
@@ -74,7 +85,7 @@ class PortFiltering(Commons):
         args = parser.parse_args()
 
         args_dict = vars(args)
-
+        self.logger.debug(f"Arguments Passed: {args_dict}")
         return args_dict
 
     def show_all_options(self):
@@ -88,9 +99,11 @@ class PortFiltering(Commons):
         user_scan_choice = input("Your choice: ")
         choices = ["0", "1", "2", "3", "4"]
         if user_scan_choice in choices:
+            self.logger.debug(f"The operation that you have decided to perform is {user_scan_choice}")
             return int(user_scan_choice)
 
         else:
+            self.logger.debug("Invalid Choice. Choose again.")
             print('Invalid Choice. Choose again')
             self.show_all_options()
 
@@ -98,65 +111,90 @@ class PortFiltering(Commons):
 
     def scan_all_ports(self, *args, **kwargs):
         host = kwargs.get("ip")
+        self.logger.debug(f"Host: {host}")
         if "/" in host:
             host = host.split("/")[0]
 
         open_ports = []
         closed_ports = []
+        self.logger.debug("Starting the port scan")
         for port in self.ports:
+            self.logger.debug(f"Port: {port}")
             scan = self.scan_port(host, port)
             if scan == -1:
-                print("Error in scanning")
+                self.logger.debug("Error occurred during scanning.")
 
             elif scan:
                 open_ports.append(port)
-
             else:
                 closed_ports.append(port)
 
+        self.logger.debug(f"Open Port: {open_ports}")
+        self.logger.debug(f"Closed Port: {closed_ports}")
+
     def resolve_domain(self, *args, **kwargs):
+        self.logger.debug(f"Domain name: {kwargs.get('domain')}")
         ip_address = self.resolve_domain_name(kwargs.get("domain"))
         if ip_address == -1:
-            print("Error in resolving domain")
+            self.logger.debug("Error in resolving domain")
 
         elif ip_address:
-            print(f"Domain Name: {kwargs.get('domain')}")
-            print(f"IP Address (IPv4): {ip_address}")
+            self.logger.debug(f"Domain Name: {kwargs.get('domain')}")
+            self.logger.debug(f"IP Address (IPv4): {ip_address}")
 
         else:
-            print(f"Resolution failed for domain {kwargs.get('domain')}")
+            self.logger.debug(f"Resolution failed for domain {kwargs.get('domain')}")
 
     def ping_scan(self, *args, **kwargs):
+        self.logger.debug(f"IP Address: {kwargs.get('ip')}")
+        self.logger.debug(f"Start IP Address: {kwargs.get('start_ip')} | End IP Address: {kwargs.get('end_ip')}")
+        self.logger.debug("Getting IPs based on inputs")
         target_ips = self.get_ip_list(kwargs.get("ip"), kwargs.get("start_ip"), kwargs.get("end_ip"))
+        self.logger.debug(f"The Target IP Addresses are: {target_ips}")
 
         if target_ips == -1:
-            print("Incorrect inputs")
-
+            self.logger.debug("Incorrect inputs")
         else:
+            self.logger.info("Starting sending IP ping")
             hosts = self.send_pings(target_ips)
 
-            print(f"Scan finished. Total targets: {len(target_ips)}")
-            print(f"Total live hosts found: {len(hosts)}")
+            self.logger.debug(f"Scan finished. Total targets: {len(target_ips)}")
+            self.logger.debug(f"Total live hosts found: {len(hosts)}")
 
-            for host in sorted(hosts, key=ipaddress.ip_address):
-                print(host)
+            self.logger.debug(f"Hosts found : {','.join(sorted(hosts, key=ipaddress.ip_address))}")
 
     def syn_port_scan_stealth_check(self, **kwargs):
+        self.logger.debug("Starting the SYN Scan or TCP health check")
         results = self.port_scan_stealth_check(kwargs.get("ip"))
         open_ports = [p for p, s in results.items() if s == 'open']
         closed_ports = [p for p, s in results.items() if s == 'closed']
         filtered_ports = [p for p, s in results.items() if s == 'filtered']
 
-        print(f"Total ports scanned: {len(self.ports)}")
+        self.logger.debug(f"Total ports scanned: {len(self.ports)}")
 
         if open_ports:
-            print(f"OPEN Ports ({len(open_ports)}): {', '.join(map(str, sorted(open_ports)))}")
+            self.logger.debug(f"OPEN Ports ({len(open_ports)}): {', '.join(map(str, sorted(open_ports)))}")
 
         if filtered_ports:
-            print(f"FILTERED Ports ({len(filtered_ports)}): {', '.join(map(str, sorted(filtered_ports)))} Firewall likely dropping packets")
+            self.logger.debug(f"FILTERED Ports ({len(filtered_ports)}): {', '.join(map(str, sorted(filtered_ports)))} Firewall likely dropping packets")
 
-        print(f"CLOSED Ports ({len(closed_ports)}): Displayed only if necessary or requested.")
+        self.logger.debug(f"CLOSED Ports ({len(closed_ports)}): Displayed only if necessary or requested.")
 
+
+def setup_logging():
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s | %(levelname)-8s | %(name)-15s | Line %(lineno)d | %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+    file_handler = logging.FileHandler("Port_Scanning.log", mode="w")
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(logging.Formatter("%(asctime)s | %(levelname)-8s | %(name)-15s | Line %(lineno)d | %(message)s"))
+
+    root_logger = logging.getLogger()
+    root_logger.addHandler(file_handler)
 
 if __name__ == '__main__':
+    setup_logging()
     PortFiltering().main()
