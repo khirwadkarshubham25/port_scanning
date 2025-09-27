13b5303c1050c3ce1ccec465f5e167b3d691b93d
Shubham Khirwadkar <khirwadkar.shubham25@gmail.com>
2025-09-27 10:00:26 -0500

    Restructure, resolve and ping scan

--- scripts/commons.py
+++ scripts/commons.py
@@ -1,15 +1,19 @@
+import concurrent.futures
+import ipaddress
 import socket
+
+import ping3
 
 
 class Commons:
     def __init__(self):
-        pass
+        self.timeout = 2
+        self.max_workers = 50
 
-    @staticmethod
-    def scan_port(host, port):
+    def scan_port(self, host, port):
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.settimeout(2)
+            s.settimeout(self.timeout)
 
             result = s.connect_ex((host, port))
 
@@ -19,13 +23,95 @@ class Commons:
             else:
                 return False
 
-        except socket.gaierror:
-            print("Hostname could not be resolved. Exiting.")
+        except socket.gaierror as e:
+            print(f"Hostname could not be resolved. Exiting\nError:\n{e}.")
             return -1
 
-        except socket.error:
-            print("Could not connect to server")
+        except socket.error as e:
+            print(f"Could not connect to server. Exiting\nError:\n{e}")
             return -1
 
         finally:
             s.close()
+
+    @staticmethod
+    def resolve_domain_name(domain):
+        try:
+            ipaddress = socket.gethostbyname(domain)
+            return ipaddress
+
+        except socket.gaierror as e:
+            print(f"Domain name {domain} could not be resolved. Exiting\nError:\n{e}.")
+            return -1
+
+        except Exception as e:
+            print(f"An unexcepted error occurred. Exiting\nError:\n{e}")
+            return -1
+
+    @staticmethod
+    def get_ip_list(ip, start_ip, end_ip):
+        ip_list = []
+        if ip is not None and "/" in ip:
+            try:
+                network = ipaddress.ip_network(ip, strict=False)
+                for ip in network.hosts():
+                    ip_list.append(str(ip))
+
+                return ip_list
+            except ValueError as e:
+                print(f"Invalid Input. Error:\n{e}")
+
+        elif ip is not None and "/" not in ip:
+            try:
+                ip = ipaddress.ip_address(ip)
+                return [ip]
+            except ValueError as e:
+                print(f"Invalid Input. Error:\n{e}")
+                return []
+
+        elif start_ip is not None and end_ip is not None:
+            try:
+                start_ip = ipaddress.ip_network(start_ip.strip())
+                end_ip = ipaddress.ip_network(end_ip.strip())
+
+                if start_ip > end_ip:
+                    start_ip, end_ip = end_ip, start_ip
+
+                while start_ip <= end_ip:
+                    ip_list.append(str(start_ip))
+                    start_ip = ipaddress.ip_address(int(start_ip) + 1)
+
+                return ip_list
+
+            except ValueError as e:
+                print(f"Invalid Input. Error:\n{e}")
+
+        else:
+            print("No input provided. Please provide input")
+            return -1
+
+    def send_pings(self, target_ips):
+        hosts = []
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.max_workers) as exec:
+            ping_all_ips = {exec.submit(self.ping_ip, ip) : ip for ip in target_ips}
+
+            for ping in ping_all_ips:
+                ip, is_reachable = ping.result()
+
+                if is_reachable:
+                    hosts.append(ip)
+
+        return hosts
+
+    def ping_ip(self, ip):
+        try:
+            delay = ping3.ping(ip, timeout=self.timeout)
+
+            is_reachable = isinstance(delay, float)
+
+            return ip, is_reachable
+
+        except Exception as e:
+            print(f"Error in ping {ip}. Error\n{e}")
+            return  -1, False
--- scripts/port_scanning.py
+++ scripts/port_scanning.py
@@ -1,40 +1,72 @@
 import argparse
-import logging
+import ipaddress
 import os
 import platform
 import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from scripts.operating_systems.linux_port_scanning import LinuxPortFiltering
+from scripts.commons import Commons
 
-class PortFiltering:
+class PortFiltering(Commons):
     def __init__(self):
-        pass
+        super().__init__()
+        self.ports = list(range(1, 65535))
 
-    def run(self):
-        options = self.take_cli_options()
-        system_name = platform.system()
-        if system_name == 'Windows':
-            print("This is a Windows machine.")
-            # Example command: os.system('dir')
-        elif system_name == 'Darwin':
-            print("This is a macOS machine.")
-            # Example command: os.system('ls')
-        elif system_name == 'Linux':
-            print("This is a Linux machine.")
-            LinuxPortFiltering(**options).run()
+    def main(self):
+        self.clear_cli()
+        cli_args = self.take_cli_options()
+        option = self.show_all_options()
+
+        if option == 0:
+            print('Exiting the scanning')
+
+        if option == 1:
+            self.scan_all_ports(**cli_args)
+
+        if option == 2:
+            self.resolve_domain(**cli_args)
+
+        if option == 3:
+            self.ping_scan(**cli_args)
+
+    @staticmethod
+    def clear_cli():
+        if platform.system() == 'Windows':
+            os.system("cls")
+
+        elif platform.system() == 'Linux' or platform.system() == 'Darwin':
+            os.system("clear")
+
         else:
-            print(f"Unknown operating system: {system_name}")
+            print(f"Unknown operating system: {platform.system()}")
 
     @staticmethod
     def take_cli_options():
         parser = argparse.ArgumentParser(description="This script is user for scanning the ports")
 
         parser.add_argument(
-            "-t_ip", "--target_ipaddress",
-            dest="target_ip_address",
+            "-ip", "--ipaddress",
+            dest="ip",
+            type=str
+        )
+
+        parser.add_argument(
+            "-d", "--domain",
+            dest="domain",
             type=str,
-            default='localhost'
+            default="www.google.com"
+        )
+
+        parser.add_argument(
+            "-s", "--startIp",
+            dest="start_ip",
+            type=str
+        )
+
+        parser.add_argument(
+            "-e", "--endIp",
+            dest="end_ip",
+            type=str
         )
 
         args = parser.parse_args()
@@ -43,5 +75,70 @@ class PortFiltering:
 
         return args_dict
 
+    def show_all_options(self):
+        print('Select the type of operation you would like to perform:\n'
+              '0: Exit\n'
+              '1: Scan All Ports or Vanilla Scan\n'
+              '2: Resolve\n'
+              '3: Ping Scan\n'
+              '4: ')
+        user_scan_choice = input("Your choice: ")
+        choices = ["0", "1", "2", "3"]
+        if user_scan_choice in choices:
+            return int(user_scan_choice)
+
+        else:
+            print('Invalid Choice. Choose again')
+            self.show_all_options()
+
+        return None
+
+    def scan_all_ports(self, *args, **kwargs):
+        host = kwargs.get("ip")
+        if "/" in host:
+            host = host.split("/")[0]
+
+        open_ports = []
+        closed_ports = []
+        for port in self.ports:
+            scan = self.scan_port(host, port)
+            if scan == -1:
+                print("Error in scanning")
+
+            elif scan:
+                open_ports.append(port)
+
+            else:
+                closed_ports.append(port)
+
+    def resolve_domain(self, *args, **kwargs):
+        ip_address = self.resolve_domain_name(kwargs.get("domain"))
+        if ip_address == -1:
+            print("Error in resolving domain")
+
+        elif ip_address:
+            print(f"Domain Name: {kwargs.get('domain')}")
+            print(f"IP Address (IPv4): {ip_address}")
+
+        else:
+            print(f"Resolution failed for domain {kwargs.get('domain')}")
+
+    def ping_scan(self, *args, **kwargs):
+        target_ips = self.get_ip_list(kwargs.get("ip"), kwargs.get("start_ip"), kwargs.get("end_ip"))
+
+        if target_ips == -1:
+            print("Incorrect inputs")
+
+        else:
+            hosts = self.send_pings(target_ips)
+
+            print(f"Scan finished. Total targets: {len(target_ips)}")
+            print(f"Total live hosts found: {len(hosts)}")
+
+            for host in sorted(hosts, key=ipaddress.ip_address):
+                print(host)
+
+
+
 if __name__ == '__main__':
-    PortFiltering().run()
+    PortFiltering().main()
